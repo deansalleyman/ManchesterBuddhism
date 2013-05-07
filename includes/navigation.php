e103703e39c482b4d7de55928f595ef838c40a5a
deansalleyman <dean@becordial.co.uk>
2013-05-07 10:18:02 +0100

    Custom nav

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,4 +1,26 @@
 <nav>
 
-<?php wp_nav_menu( array( 'theme_location' => 'main_menu' ) ); ?>
+<?php 
+
+$args = array(
+	'theme_location'  => 'main_menu',
+	'menu'            => 'main_menu',
+	'container'       => 'div',
+	'container_class' => 'navbar',
+	'container_id'    => '',
+	'menu_class'      => 'nav',
+	'menu_id'         => '',
+	'echo'            => true,
+	'fallback_cb'     => 'wp_page_menu',
+	'before'          => '',
+	'after'           => '',
+	'link_before'     => '',
+	'link_after'      => '',
+	'items_wrap'      => '<div class="navbar-inner"><ul id="%1$s" class="%2$s">%3$s</ul></div>',
+	'depth'           => 0,
+	'walker'          => new MV_Cleaner_Walker_Nav_Menu()
+);
+
+
+wp_nav_menu($args); ?>
 </nav>
